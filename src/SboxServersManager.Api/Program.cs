49006a5e3d9679332464230ed3edaa84587c689b
Vladislav Kurbatov <gromazekazeka@gmail.com>
2024-09-09 16:54:10 +0300

    add Serilog

--- src/SboxServersManager.Api/Program.cs
+++ src/SboxServersManager.Api/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using SboxServersManager.Api.Extensions;
 using SboxServersManager.Application;
 using SboxServersManager.Infrastructure;
+using SboxServersManager.Infrastructure.Logging.Configutations;
+using Serilog;
 
 namespace SboxServersManager.Api
 {
@@ -10,34 +12,53 @@ namespace SboxServersManager.Api
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-            builder.Services.ConfigurationCors();
-            builder.Services.ConfigureVersioning();
-
-            builder.Services.AddApplication();
-            builder.Services.AddInfrastructure(builder.Configuration);
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.ConfigurationSerilog();
+
+                builder.Services.ConfigurationCors();
+                builder.Services.ConfigureVersioning();
+
+                builder.Services.AddApplication();
+                builder.Services.AddInfrastructure(builder.Configuration);
+
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseSerilogRequestLogging();
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "server terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
---
+++ src/SboxServersManager.Infrastructure/Logging/Configutations/LoggerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SboxServersManager.Infrastructure.Logging.Configutations
+{
+    public static class LoggerExtensions
+    {
+        public static IHostBuilder ConfigurationSerilog(this IHostBuilder hostBuilder)
+        {
+            hostBuilder.UseSerilog((context, logger) =>
+            {
+                logger.ReadFrom.Configuration(new ConfigurationBuilder()
+                        .AddJsonFile("serilog.config.json")
+                        .Build())
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+            });
+
+            return hostBuilder;
+        }
+    }
+}
